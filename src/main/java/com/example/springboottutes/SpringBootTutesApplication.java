d74ba6a1bad1d96d4f6579f8afafd046bfeb4032
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-07-26 17:17:45 +0530

    first commit

---
+++ src/main/java/com/example/springboottutes/SpringBootTutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTutesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottutes/SpringBootTutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
