5713144434798fd3ef7facb892d220aab5a2cd4e
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-07-28 04:51:07 +0530

    @Sequence

--- src/main/java/com/example/springboottutes/Student.java
+++ src/main/java/com/example/springboottutes/Student.java
@@ -1,12 +1,20 @@
 package com.example.springboottutes;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "Student")
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
